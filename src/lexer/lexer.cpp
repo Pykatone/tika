2c3423ae037084c09eef118e650acd3c08f9a2d3
ichuusy <83366765+ichuusy@users.noreply.github.com>
2023-08-17 00:45:20 +0300

    Major update
    Keywords added and can identifier in Lexer.
    Variable types and more type added. Mathematical operators and condition operators added.
    Added GetNextChar and this function gives next char in text.

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -6,6 +6,33 @@ enum TokenType
     PLUS,
     MINUS,
     MULTIPLY,
+    INTEGER,
+    FLOAT,
+    STR,
+    BOOL,
+    EQUALITY,
+    INEQUALITY,
+    GREATER,
+    LESS,
+    AND,
+    OR,
+    NOT,
+    IF,
+    ELSEIF,
+    ELSE,
+    FOR,
+    WHILE,
+    LOOP,
+    ENDLOOP,
+    CONTINUE,
+    BREAK,
+    STRUCT,
+    ENUM,
+    FUNC,
+    RETURNTYPE,
+    RETURN,
+    NONE,
+    NEWLINE,
     DIVIDE,
     MODULO,
     LPAREN,
@@ -15,8 +42,8 @@ enum TokenType
     RPAREN,
     HASHTAG,
     SEMICOLON,
-    LESSTHAN,
-    GREATERTHAN,
+    LESSEQUAL,
+    GREATEREQUAL,
     EQUAL,
     COMMENT,
     SLASH,
@@ -29,6 +56,15 @@ enum TokenType
     RCURLY,
     LSQUARE,
     RSQUARE,
+    STRING,
+    INCREMENT,
+    DECREMENT,
+    ADDITIONASSIGNMENT,
+    SUBTRACTIONASSIGNMENT,
+    MULTIPLICATIONASSIGNMENT,
+    DIVISIONASSIGNMENT,
+    MODULUSASSIGNMENT,
+    IMPORT,
     END_OF_FILE
 };
 struct Token
@@ -37,10 +73,15 @@ struct Token
     std::string value;
 };
 
-const char* tokens[]{ "PLUS" , "MINUS", "MULTIPLY", "DIVIDE","MODULO", "LPAREN", "SPACE", "NUMBER",
-                      "IDENTIFIER", "RPAREN","HASHTAG","SEMICOLON","LESSTHAN","GREATERTHAN",
+const char* tokens[]{ "PLUS" , "MINUS", "MULTIPLY","INTEGER","FLOAT","STRING","BOOL","EQUALITY",
+                      "INEQUALITY","GREATER","LESS","AND","OR","NOT","IF","ELSEIF","ELSE","FOR",
+                      "WHILE","LOOP","ENDLOOP","CONTINUE","BREAK","STRUCT","ENUM","FUNCTION",
+                      "RETURNTYPE","RETURN","NONE","NEWLINE", "DIVIDE","MODULO", "LPAREN", "SPACE", "NUMBER",
+                      "IDENTIFIER", "RPAREN","HASHTAG","SEMICOLON","LESSEQUAL","GREATEREQUAL",
                       "EQUAL","COMMENT","SLASH","SINGLEQUOTE","DOUBLEQUOTE","COMMA","DOT",
-                      "COLON","LCURLY","RCURLY","LSQUARE","RSQUARE", "END_OF_FILE"};
+                      "COLON","LCURLY","RCURLY","LSQUARE","RSQUARE","STRING","INCREMENT",
+                      "DECREMENT","ADDITIONASSIGNMENT","SUBTRACTIONASSIGNMENT","MULTIPLICATIONASSIGNMENT",
+                      "DIVISIONASSIGMENT","MODULUSASSIGNMENT","IMPORT", "END_OF_FILE" };
 class Lexer
 {
 public:
@@ -66,25 +107,32 @@ public:
         {
         case '+':
             pos++;
+            if (getNextChar() == '+') { pos++; return { INCREMENT, "++" }; break; }
+            else if (getNextChar() == '=') { pos++; return { ADDITIONASSIGNMENT, "+=" }; break; }
             return { PLUS, "+" };
             break;
 
         case '-':
             pos++;
+            if (getNextChar() == '-') { pos++; return { DECREMENT, "--" }; break; }
+            else if (getNextChar() == '=') { pos++; return { SUBTRACTIONASSIGNMENT, "-=" }; break; }
             return { MINUS, "-" };
             break;
 
         case '/':
             pos++;
+            if (getNextChar() == '=') { pos++; return { DIVISIONASSIGNMENT, "/=" }; break; }
             return { DIVIDE, "/" };
             break;
         case '%':
             pos++;
+            if (getNextChar() == '=') { pos++; return { MODULUSASSIGNMENT, "%=" }; break; }
             return { MODULO, "%" };
             break;
 
         case '*':
             pos++;
+            if (getNextChar() == '=') { pos++; return { MULTIPLICATIONASSIGNMENT, "%=" }; break; }
             return { MULTIPLY, "*" };
             break;
 
@@ -104,7 +152,7 @@ public:
             break;
         case '\n':
             pos++;
-            return { SPACE, "\\n" };
+            return { NEWLINE, "\\n" };
             break;
         case ';':
             pos++;
@@ -112,23 +160,48 @@ public:
             break;
         case '<':
             pos++;
-            return { LESSTHAN, "<" };
+            if (getNextChar() == '=')
+            {
+                pos++;
+                return { LESSEQUAL, "<=" };
+                break;
+            }
+
+            return { LESS, "<" };
             break;
         case '>':
             pos++;
-            return { GREATERTHAN, ">" };
+            if (getNextChar() == '=')
+            {
+                pos++;
+                return { GREATEREQUAL, "<=" };
+                break;
+            }
+            return { GREATER, ">" };
             break;
         case '=':
             pos++;
+            if (getNextChar() == '=')
+            {
+                pos++;
+                return { EQUALITY, "==" };
+                break;
+            }
+            else if (getNextChar() == '!')
+            {
+                pos++;
+                return { INEQUALITY, "=!" };
+                break;
+            }
             return { EQUAL, "=" };
             break;
         case '\'':
             pos++;
             return { SINGLEQUOTE, "'" };
             break;
-        case '"':
+        case '\"':
             pos++;
-            return { DOUBLEQUOTE, "\"" };
+            return LiteralString();
             break;
         case '.':
             pos++;
@@ -164,6 +237,37 @@ public:
 
     }
 private:
+    char getNextChar()
+    {
+        char nextChar = text[pos];
+        return nextChar;
+    }
+    Token Identifier(const std::string& text)
+    {
+        if (text == "int") return { INTEGER, "INTEGER" };
+        else if (text == "float") return { FLOAT, "FLOAT" };
+        else if (text == "string") return { STR, "STR" };
+        else if (text == "bool") return { BOOL, "BOOLEAN" };
+        else if (text == "and") return { AND, "AND" };
+        else if (text == "or") return { OR, "OR" };
+        else if (text == "not") return { NOT, "NOT" };
+        else if (text == "if") return { IF, "IF" };
+        else if (text == "elseif") return { ELSEIF, "ELSEIF" };
+        else if (text == "else") return { ELSE, "ELSE" };
+        else if (text == "for") return { FOR, "FOR" };
+        else if (text == "while") return { WHILE, "WHILE" };
+        else if (text == "continue") return { CONTINUE, "CONTINUE" };
+        else if (text == "break") return { BREAK, "BREAK" };
+        else if (text == "struct") return { STRUCT, "STRUCT" };
+        else if (text == "enum") return { ENUM, "ENUM" };
+        else if (text == "fn") return { FUNC, "FUNCTION" };
+        else if (text == "return") return { RETURN, "RETURN" };
+        else if (text == "none") return { NONE, "NULL" };
+        else if (text == "true") return { BOOL, "TRUE" };
+        else if (text == "false") return { BOOL, "FALSE" };
+        else if (text == "include") return { IMPORT, "INCLUDE" };
+        else return { IDENTIFIER, text };
+    }
     Token isDigit()
     {
         std::string result = "";
@@ -182,7 +286,19 @@ private:
             result += text[pos];
             pos++;
         }
-        return { IDENTIFIER, result };
+        return Identifier(result);
+    }
+    Token LiteralString()
+    {
+        std::string result = "";
+        while (pos <= text.length() && text[pos] != '\"')
+        {
+            result += text[pos];
+            pos++;
+        }
+        pos++;
+        if (result == "\"") return { DOUBLEQUOTE, "\"" };
+        return { STRING, result };
     }
     Token isComment()
     {
@@ -211,26 +327,31 @@ private:
 
 int main()
 {
-    auto input =
-        "def Fibonacci(n): ## Recursive Function created.\n"
-        "if (n <= 1) : ## If n smaller than 1 returned 1\n"
-        "return 1\n"
-        "else: ## Else n* (n - 1)\n"
-        "return (n * Fibonacci(n - 1))\n"
-        "n = Fibonacci(int(input('Write number of fibonacci : ')))\n"
-        "print(n)";
-
+    auto input = "fn merhaba(x,y):\n"
+        "int test = 30\n"
+        "return test+30\n"
+        "string merhaba = \"test selam not or false merhaba and and and =>\"\n"
+        "bool merhaba = true;\n"
+        "for(int x = 0; x <= 30; x++)\n"
+        "if(x == test): return true\n"
+        "print(\"Sifirdan programlama dili gelistiriyoruz.\")\n"
+        "x++;"
+        "y+=30;"
+        "c %= 2"
+        "if(c % 2 == 0) return true;"
+        "while (true): break;"
+        "include discord";
 
     /*std::regex pattern("\\s+");
     std::string result = std::regex_replace(input, pattern, "");*/
     Lexer lexer(input);
     Token token = lexer.getNextToken();
-    while(token.type != END_OF_FILE) 
+    while (token.type != END_OF_FILE)
     {
-        if(token.type != SPACE) std::cout << tokens[token.type] << " |" << token.value << "| " << std::endl;
+        if (token.type != SPACE) std::cout << tokens[token.type] << " |" << token.value << "| " << std::endl;
         token = lexer.getNextToken();
     }
-    
+    std::cout << tokens[TokenType::END_OF_FILE];
     return 0;
 }
 
