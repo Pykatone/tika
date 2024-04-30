ae97a96cea3e7c060aa7f2f475a1755abe2cb01b
ichuusy <83366765+ichuusy@users.noreply.github.com>
2024-04-30 17:13:15 +0300

    Added Lexer.cs
    Lexer rewriting with C#.

---
+++ src/lexer/lexer.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace Lexer
+{
+    enum TokenType
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Integer,
+        Float,
+        String,
+        Bool,
+        Equality,
+        Inequality,
+        Greater,
+        Less,
+        And,
+        Or,
+        Not,
+        If,
+        Elif,
+        Else,
+        For,
+        While,
+        Loop,
+        EndLoop,
+        Continue,
+        Break,
+        Enum,
+        Function,
+        Return,
+        ReturnType,
+        None,
+        NewLine,
+        Divide,
+        Modulo,
+        LParen,
+        RParen,
+        Space,
+        Number,
+        Text,
+        Identifier,
+        Semicolon,
+        LessEqual,
+        GreaterEqual,
+        Equal,
+        Comment,
+        Slash,
+        SingleQuote,
+        DoubleQuote,
+        Comma,
+        Dot,
+        LCurly,
+        RCurly,
+        LSquare,
+        RSquare,
+        Increment,
+        Decrement,
+        ModuloAssignment,
+        AdditionAssignment,
+        DivisonAssignment,
+        SubtractionAssignment,
+        MultiplicationAssignment
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }    
+}
